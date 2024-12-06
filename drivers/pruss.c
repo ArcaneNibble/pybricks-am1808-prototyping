03c007047897747e36a81bdbd8594b79e6886408
Laurens Valk <laurens@pybricks.com>
2024-12-06 09:41:57 +0100

    drivers/pruss: Don't include am335x files.
    We don't have this.

--- drivers/pruss.c
+++ drivers/pruss.c
@@ -42,7 +42,6 @@
 
 #include "hw/hw_types.h"
 #include "pruss.h"
-#include "armv7a/am335x/interrupt.h"
 
 /*****************************************************************************/
 /*                      INTERNAL MACRO DEFINITIONS                           */
